b13b95474e2abc793b5d7cc2a0501f238efddb85
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-10-16 22:31:25 +0300

    Enabling

--- buses_api/views.py
+++ buses_api/views.py
@@ -49,7 +49,7 @@ def import_from_kobo(request):
             sent_on = dateutil.parser.parse(d['_submission_time'])
             sent_on = sent_on + timedelta(hours=3)
 
-            for c in models.SmsReceiver.objects.filter(destination__in=destinations):
+            for c in models.SmsReceiver.objects.filter(destination__in=destinations, enabled=True):
                 twilio.messages.create(from_="IRC", to=c.phone_number,
                                        body="A bus has been dispatched to {} at {}."
                                        .format(destination_friendly[c.destination], sent_on.strftime("%H:%M:%S")))
